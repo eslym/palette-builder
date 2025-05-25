521829556a516ff5513fb83b251037fab6824aa4
0nepeop1e <engshun.lim@gmail.com>
2025-05-25 21:53:55 +0800

    enhance result generation

--- src/lib/components/panes/ResultPane.svelte
+++ src/lib/components/panes/ResultPane.svelte
@@ -1,40 +1,119 @@
-<script lang="ts">
-	import * as Tabs from '$lib/components/ui/tabs';
-	import * as Card from '$lib/components/ui/card';
-	import { type Snippet } from 'svelte';
-	import { local } from '$lib/store';
-	import { camelCase, kebabCase } from 'lodash-es';
-
-	let { colors }: { colors: ColorRecord[] } = $props();
-
-	function css_value(record: ColorRecord) {
+<script lang="ts" module>
+	function light_dark(record: ColorRecord) {
 		if (record.lightDark) {
 			return `light-dark(${record.light}, ${record.dark})`;
 		}
 		return record.light;
 	}
 
-	function css_code(colors: ColorRecord[]) {
-		const lines = [':root {'];
-		for (const record of colors) {
-			lines.push(`\t--${record.name}: ${css_value(record)};`);
-		}
-		lines.push('}', '');
-		return lines.join('\n');
-	}
+	const cases = new Map([
+		[null, { label: 'None', fn: (str: string) => str }],
+		['camel', { label: 'Camel Case', fn: (str: string) => camelCase(str) }],
+		['kebab', { label: 'Kebab Case', fn: (str: string) => kebabCase(str) }],
+		['snake', { label: 'Snake Case', fn: (str: string) => snakeCase(str) }]
+	]);
 
-	function stylex_code(colors: ColorRecord[]) {
-		const lines = [
-			'import * as stylex from "@stylexjs/stylex";',
-			'',
-			'export const colors = stylex.defineVars({'
-		];
-		for (const record of colors) {
-			lines.push(`\t${record.name}: ${JSON.stringify(css_value(record))},`);
-		}
-		lines.push('});', '');
-		return lines.join('\n');
-	}
+	const darkMode = new Map([
+		[
+			null,
+			{
+				label: 'light-dark function',
+				css(colors: ColorRecord[], case_convert: (str: string) => string) {
+					const lines = [':root {'];
+					for (const record of colors) {
+						lines.push(`\t--${case_convert(record.name)}: ${light_dark(record)};`);
+					}
+					lines.push('}', '');
+					return lines.join('\n');
+				},
+				stylex(colors: ColorRecord[], case_convert: (str: string) => string) {
+					const lines = ['export const colors = stylex.defineVars({'];
+					for (const record of colors) {
+						lines.push(
+							`\t${case_convert(record.name)}: light_dark(${JSON.stringify(record.light)}, ${JSON.stringify(record.dark)}),`
+						);
+					}
+					lines.push('});', '');
+					return lines.join('\n');
+				}
+			}
+		],
+		[
+			'media',
+			{
+				label: 'Media Query',
+				css(colors: ColorRecord[], case_convert: (str: string) => string) {
+					const root = [':root {'];
+					const media = ['@media (prefers-color-scheme: dark) {', '\t:root {'];
+					for (const record of colors) {
+						root.push(`\t--${case_convert(record.name)}: ${record.light};`);
+						media.push(`\t\t--${case_convert(record.name)}: ${record.dark};`);
+					}
+					root.push('}', '');
+					media.push('\t}', '}', '');
+					return root.concat(media).join('\n');
+				},
+				stylex(colors: ColorRecord[], case_convert: (str: string) => string) {
+					const lines = ['export const colors = stylex.defineVars({'];
+					for (const record of colors) {
+						lines.push(
+							`\t${case_convert(record.name)}: {`,
+							`\t\tdefault: ${JSON.stringify(record.light)},`,
+							`\t\t'@media (prefers-color-scheme: dark)': ${JSON.stringify(record.dark)}`,
+							'\t},'
+						);
+					}
+					lines.push('});', '');
+					return lines.join('\n');
+				}
+			}
+		],
+		[
+			'class',
+			{
+				label: 'Class',
+				css(colors: ColorRecord[], case_convert: (str: string) => string) {
+					const root = [':root {'];
+					const kelas = ['.dark {'];
+					for (const record of colors) {
+						root.push(`\t--${case_convert(record.name)}: ${record.light};`);
+						kelas.push(`\t--${case_convert(record.name)}: ${record.dark};`);
+					}
+					root.push('}', '');
+					kelas.push('}', '');
+					return root.concat(kelas).join('\n');
+				},
+				stylex(colors: ColorRecord[], case_convert: (str: string) => string) {
+					const vars = ['export const colors = stylex.defineVars({'];
+					const theme = ['export const darkTheme = stylex.createTheme(colors, {'];
+					for (const record of colors) {
+						vars.push(`\t${case_convert(record.name)}: ${JSON.stringify(record.light)},`);
+						theme.push(`\t${case_convert(record.name)}: ${JSON.stringify(record.dark)},`);
+					}
+					vars.push('});', '');
+					theme.push('});', '');
+					return vars.concat(theme).join('\n');
+				}
+			}
+		]
+	]);
+</script>
+
+<script lang="ts">
+	import * as Tabs from '$lib/components/ui/tabs';
+	import * as Card from '$lib/components/ui/card';
+	import { type Snippet } from 'svelte';
+	import { local } from '$lib/store';
+	import { camelCase, kebabCase, snakeCase } from 'lodash-es';
+	import { Label } from '$lib/components/ui/label';
+	import * as Select from '$lib/components/ui/select';
+
+	let { colors }: { colors: ColorRecord[] } = $props();
+
+	let css_case = $derived(cases.get(local['css-case'])!.fn);
+	let stylex_case = $derived(cases.get(local['stylex-case'])!.fn);
+
+	$inspect(css_case, stylex_case);
 </script>
 
 {#snippet render_tab(key: string, children: Snippet)}
@@ -47,12 +126,47 @@
 	</Tabs.Content>
 {/snippet}
 
+{#snippet controls(type: 'css' | 'stylex')}
+	<div class="mb-6 flex flex-row flex-wrap gap-x-6 gap-y-2">
+		<div class="flex flex-row items-center gap-2">
+			<Label for="{type}-case">Case Convert</Label>
+			<Select.Root type="single" bind:value={local[`${type}-case`]!}>
+				<Select.Trigger id="{type}-case" class="w-36">
+					{cases.get(local[`${type}-case`])!.label}
+				</Select.Trigger>
+				<Select.Content>
+					{#each Array.from(cases.entries()) as [value, { label }]}
+						<Select.Item value={value!}>{label}</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
+		</div>
+		<div class="flex flex-row items-center gap-2">
+			<Label for="{type}-dark-mode">Dark Mode</Label>
+			<Select.Root type="single" bind:value={local[`${type}-dark-mode`]!}>
+				<Select.Trigger id="{type}-dark-mode" class="w-44">
+					{darkMode.get(local[`${type}-dark-mode`])!.label}
+				</Select.Trigger>
+				<Select.Content>
+					{#each Array.from(darkMode.entries()) as [value, { label }]}
+						<Select.Item value={value!}>{label}</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
+		</div>
+	</div>
+{/snippet}
+
 {#snippet css_tab()}
-	<pre>{css_code(colors)}</pre>
+	{@const dark_mode = darkMode.get(local['css-dark-mode'])!}
+	{@render controls('css')}
+	<pre>{dark_mode.css(colors, css_case)}</pre>
 {/snippet}
 
 {#snippet stylex_tab()}
-	<pre>{stylex_code(colors)}</pre>
+	{@const dark_mode = darkMode.get(local['stylex-dark-mode'])!}
+	{@render controls('stylex')}
+	<pre>{dark_mode.stylex(colors, stylex_case)}</pre>
 {/snippet}
 
 <Tabs.Root
